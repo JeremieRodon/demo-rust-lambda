5c9ff2b1ee2c3c82a782727b9a40e4cf278d76e6
Jérémie Rodon <jeremie.rodon@gmail.com>
2025-06-01 09:42:31 +0200

    Moving Nix flakes in a subdirectory to avoid caching the entire codebase in the store (and lifting the .gitignore on it

---
+++ nix/flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [rustToolchain jdk21 python312Packages.boto3];
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+        }
+    );
+}
